d8897287ab70a8f83233f38e038de669311deaff
Zarif011202009 <szarif202009@bscse.uiu.ac.bd>
2023-06-10 01:26:52 +0600

    Homepage design commit by Zarif

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::view('homepage', 'homepage');
+
+Route::get('signup', function () {       
+    return view('signup');
+});
+
+Route::get('login', function () {       
+    return view('login');
+});
+
+Route::get('login_admin', function () {       
+    return view('login_admin');
+});
+
+Route::get('about_us', function () {       
+    return view('about_us');
+});
+
+
