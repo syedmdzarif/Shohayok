0e301b6a3f512ec89c0e1e687be8a8a278be0f6a
Zarif011202009 <szarif202009@bscse.uiu.ac.bd>
2023-06-29 23:38:50 +0600

    CSS commit by Zarif

--- resources/views/upload_content.blade.php
+++ resources/views/upload_content.blade.php
@@ -1,18 +1,32 @@
-<html>
-<h1>upload page</h1>
-<a class="e_login" href="{{url('profile_user')}}">Home</a>
-<h3>{{\Auth::user()->name}}</h3>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="/css/upload_content.css">
+    <title>Conversation</title>
+</head>
+<body>
 
 
+<div class="form">
 <form action="{{url('/upload_backend')}}" method="post" enctype="multipart/form-data">
-
+    
     @csrf
+    <label class="up">Upload Content</label>
+    <br>
     <input type="text" name="title" placeholder="Enter content title">
-    <input type="text" name="description" placeholder="Enter desctiption">
+    <input type="text" name="description" placeholder="Enter description">
     <input type="file" name="file" placeholder="Choose file">
-    <button type="submit">Upload</button>
+    <button type="submit" class="btn">Upload</button>
 
-<form>
+</form>
+<a href="{{url('/profile_user')}}"><label class="back">Go back</label></a>
+
+</div>
+
+</body>
 
 </html>
 
